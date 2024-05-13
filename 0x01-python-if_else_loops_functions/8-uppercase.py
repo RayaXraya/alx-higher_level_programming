e092c2fdca2e5768b5ba9a85aa9ebf1452d12d1a
RayaXraya <rrayanmr2003@gmail.com>
2024-05-13 03:49:38 -0700

    l

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+#Author - Rayan
+
+def islower(c):
+    """Checks for lowercase"""
+    if ord(c) >= 97 and ord(c) <= 122:
+        return True
+    else:
+        return False
+
+
+def uppercase(str):
+    """turn into upper case letter"""
+    for i in str:
+        print("{:c}"
+            .format(ord(i) if not islower(i) else ord(i) - 32),
+            end="")
+    print("")
