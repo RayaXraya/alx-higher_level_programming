7a6f174af7f909ed5c73c4f039bdc9c2c5b52c2b
RayaXraya <rrayanmr2003@gmail.com>
2024-05-13 03:35:24 -0700

    l

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -2,8 +2,8 @@
 # Author - Rayan Mozamel
 
 def islower(c):
-  """Checks for lowercase"""
-  if ord(c) >= 97 and ord(c) <= 122:
-    return True
-  else:
-    return False
+    """Checks for lowercase"""
+    if ord(c) >= 97 and ord(c) <= 122:
+        return True
+    else:
+        return False
